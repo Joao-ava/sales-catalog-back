3228bb83af1bd7523e1d1a0b7d35b3b8e1bba49e
Andressa Evelyn <andressaevelyn200214@gmail.com>
2025-05-02 22:04:44 -0300

    cadastro produto

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,5 +1,30 @@
+import Product from '../models/Product.js';
+
 class ProductsController {
   async add(req, res) {
+    const { nome, imagem, preco } = req.body;
+
+    if (!nome || !imagem || !preco) {
+      return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+    }
+
+    try{
+
+      const novoProduto = await Product.create({
+        nome,
+        imagem,
+        preco
+      });
+
+      return res.status(201).json({
+        message: 'Produto cadastrado com sucesso!',
+        produto: novoProduto
+      });
+
+    } catch(error){
+      return res.status(500).json({ message: "Erro interno no servidor.", error });
+    }
+
     return res.json()
   }
 }
---
+++ src/models/Product.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  imagem: { type: String, required: true }, 
+  preco: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model('Product', productSchema);
